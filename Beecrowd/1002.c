3fba640a7f03b882d67c438dba3099fff6d6137a
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 06:51:49 +0600

    Create 1002.c

---
+++ Beecrowd/1002.c
@@ -0,0 +1,13 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1002
+//problem name:Area of a Circle
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    double R, A;
+    scanf("%lf", &R);
+    A = 3.14159 * R * R;
+    printf("A=%0.4lf\n", A);
+    return 0;
+}
