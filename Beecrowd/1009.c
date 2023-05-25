276885469682ef1f119bebc151e07ebfddd10da7
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 07:05:28 +0600

    Create 1009.c

---
+++ Beecrowd/1009.c
@@ -0,0 +1,16 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1009
+//problem name:Salary with Bonus
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    char string[12];
+    float salary, sell;
+    scanf("%s", string);
+    scanf("%f", &salary);
+    scanf("%f", &sell);
+    double total = salary + (sell * 0.15);
+    printf("TOTAL = R$ %0.2lf\n", total);
+    return 0;
+}
