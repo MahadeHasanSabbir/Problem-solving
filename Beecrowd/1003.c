ad75b5c876824a3c744abc1fef8ee868ea78cc88
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 06:54:06 +0600

    Create 1003.c

---
+++ Beecrowd/1003.c
@@ -0,0 +1,14 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1003
+//problem name:Simple Sum
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B, SOMA;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    SOMA = A + B;
+    printf("SOMA = %d\n", SOMA);
+    return 0;
+}
