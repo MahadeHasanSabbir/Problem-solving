a3e81f70c42c4561fb23dc817d9ddffbee893df8
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 07:03:13 +0600

    Create 1008.c

---
+++ Beecrowd/1008.c
@@ -0,0 +1,17 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1008
+//problem name:Salary
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int n, h;
+    float s, total;
+    scanf("%d", &n);
+    scanf("%d", &h);
+    scanf("%f", &s);
+    total = s * h;
+    printf("NUMBER = %d\n", n);
+    printf("SALARY = U$ %0.2f\n", total);
+    return 0;
+}
