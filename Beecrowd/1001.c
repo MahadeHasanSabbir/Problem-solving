4731e58004187215fec9b90520e7ac6e39c32a32
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 06:48:07 +0600

    Create 1001.c

---
+++ Beecrowd/1001.c
@@ -0,0 +1,17 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1001
+//problem name:Extremely Basic
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B, X;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    X = A + B;
+    
+    printf("X = %d\n", X);
+    
+    return 0;
+}
