24d6947de402a8844be00d484a727ede79474d31
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 07:07:11 +0600

    Create 1010.c

---
+++ Beecrowd/1010.c
@@ -0,0 +1,19 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1010
+//problem name:Simple Calculate
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int p1, p2, q1, q2;
+    float pprice1, pprice2;
+    scanf("%d", &p1);
+    scanf("%d", &q1);
+    scanf("%f", &pprice1);
+    scanf("%d", &p2);
+    scanf("%d", &q2);
+    scanf("%f", &pprice2);
+    double total = (q1 * pprice1) + (q2 * pprice2);
+    printf("VALOR A PAGAR: R$ %0.2lf\n", total);
+    return 0;
+}
