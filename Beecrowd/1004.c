89886a191c1d9b0d5145a483c8631361689d603e
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 06:56:11 +0600

    Create 1004.c

---
+++ Beecrowd/1004.c
@@ -0,0 +1,14 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1004
+//problem name:Simple Product
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B, PROD;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    PROD = A * B;
+    printf("PROD = %d\n", PROD);
+    return 0;
+}
