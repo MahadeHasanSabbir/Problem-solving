c3f0fa31e30ab6f19acf7943628ea0f7ebe8cfaf
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 07:01:25 +0600

    Create 1007.c

---
+++ Beecrowd/1007.c
@@ -0,0 +1,16 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1007
+//problem name:Difference
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B, C, D, diff;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    diff = ((A * B) - (C * D));
+    printf("DIFERENCA = %d\n", diff);
+    return 0;
+}
