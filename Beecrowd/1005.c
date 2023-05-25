0692e175da3d422772dbdb9b40b6524354e5bf71
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-05-25 06:58:00 +0600

    Create 1005.c

---
+++ Beecrowd/1005.c
@@ -0,0 +1,14 @@
+//problem link:https://www.beecrowd.com.br/judge/en/problems/view/1005
+//problem name:Average 1
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    float A, B;
+    scanf("%f", &A);
+    scanf("%f", &B);
+    float aver = ((A * 3.5) + (B * 7.5)) / 11.0;
+    printf("MEDIA = %0.5f\n", aver);
+    return 0;
+}
