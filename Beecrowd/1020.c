212279df0c1fd071268a2849696903f9f7485468
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:41:03 +0600

    Create 1020.c

---
+++ Beecrowd/1020.c
@@ -0,0 +1,19 @@
+//problen link: https://www.beecrowd.com.br/judge/en/problems/view/1020
+//problem name: Age in Days
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int y, m, d;
+    scanf("%d", &d);
+    y = (d / 365);
+    printf("%d ano(s)\n", y);
+    d -= (y * 365);
+    m = (d / 30);
+    printf("%d mes(es)\n", m);
+    d -= (m * 30);
+    printf("%d dia(s)\n", d);
+    
+    return 0;
+}
