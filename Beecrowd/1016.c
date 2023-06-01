49dd4cceca2cb924a058055ff2f982c60735808f
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:32:28 +0600

    Create 1016.c

---
+++ Beecrowd/1016.c
@@ -0,0 +1,12 @@
+//problem link: https://www.beecrowd.com.br/judge/en/problems/view/1016
+//problem name: Distance
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d minutos\n", (n * 2));
+    return 0;
+}
