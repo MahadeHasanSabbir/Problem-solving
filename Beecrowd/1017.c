9c3284f76a5181b55e3a68286864a053689a3a6e
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:34:52 +0600

    Create 1017.c

---
+++ Beecrowd/1017.c
@@ -0,0 +1,13 @@
+//problem link: https://www.beecrowd.com.br/judge/en/problems/view/1017
+//problem name: Fuel Spent
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int h, s;
+    scanf("%d%d", &h, &s);
+    float d = (h * s) / 12.0;
+    printf("%0.3f\n", d);
+    return 0;
+}
