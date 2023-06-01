1c6ce1d92f81d4b5cd8710f62b459e006ac4ff3d
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:28:26 +0600

    Create 1014.c

---
+++ Beecrowd/1014.c
@@ -0,0 +1,13 @@
+//problem link: https://www.beecrowd.com.br/judge/en/problems/view/1014
+//problem name: Consumption
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int X;
+    float Y;
+    scanf("%d%f", &X, &Y);
+    printf("%0.3f km/l\n", (X / Y));
+    return 0;
+}
