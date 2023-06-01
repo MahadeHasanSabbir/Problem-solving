0b72aa9277337970c9d931b365d59f636cd75f98
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:30:47 +0600

    Create 1015.c

---
+++ Beecrowd/1015.c
@@ -0,0 +1,15 @@
+//problem link: https://www.beecrowd.com.br/judge/en/problems/view/1015
+//problem name: Distance Between Two Points
+//M.H.Sabbir
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x1, x2, y1, y2, distance;
+    scanf("%lf%lf", &x1, &y1);
+    scanf("%lf%lf", &x2, &y2);
+    distance = sqrt(((x2 - x1) * (x2 - x1)) + (y2 - y1) * (y2 - y1));
+    printf("%0.4lf\n", distance);
+    return 0;
+}
