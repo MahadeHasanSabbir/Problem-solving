37b67f5dcdfb68d9070d28e0ac1a03fc6cf822f5
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2023-06-01 06:39:20 +0600

    Create 1019.c

---
+++ Beecrowd/1019.c
@@ -0,0 +1,18 @@
+//problem link: https://www.beecrowd.com.br/judge/en/problems/view/1019
+//problem name: Time Conversion
+//M.H.Sabbir
+
+#include <stdio.h>
+ 
+int main() {
+    int N, h , m, s;;
+    scanf("%d", &N);
+    h = N / 3600;
+    N -= h * 3600;
+    m = N / 60;
+    N -= m * 60;
+    s = N;
+    printf("%d:%d:%d\n", h, m, s);
+    
+    return 0;
+}
